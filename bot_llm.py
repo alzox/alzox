7837797dd94494cff82c801afbb9846e1fca818b
You Gao <djx3rn@virginia.edu>
2025-04-07 21:34:05 -0400

    feat: template set-up

--- bot_llm.py
+++ bot_llm.py
@@ -3,18 +3,19 @@ import os
 from llama_cpp import Llama
 
 llm = Llama.from_pretrained(
-    repo_id="TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
+    repo_id="Qwen/Qwen2-0.5B-Instruct-GGUF",
     filename="*q8_0.gguf",
     verbose=False
 )
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 LLM_CONTEXT = """ 
-You are a sarcastic and rude AI.
-Respond to the user to make them angry.
-Don't include quotes.
 """
-
+DONT_REPLY = [
+    "Sorry, but I can't assist with that.",
+    "I'm sorry, but I can't assist with that.",
+    ""
+]
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -22,7 +23,12 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         channel = message.channel
         user_message = message.content
+        
         if message.author == self.user:
+            return
+        if len(user_message) == 1:
+            print(f"User message is too short: {user_message}") 
+            
             return
 
         response = llm.create_chat_completion(
@@ -30,9 +36,14 @@ class MyClient(discord.Client):
                 {"role": "user", "content": user_message},
                 {"role": "assistant", "content": LLM_CONTEXT}
             ],
-            max_tokens=50,
+            max_tokens=200,
         )
         response = response['choices'][0]['message']['content'].strip()
+        
+        if response in DONT_REPLY:
+            print(f"Response is in DONT_REPLY: {response}")
+            return
+        
         await channel.send(response)
         print(f'Sent message: {response}')
          
