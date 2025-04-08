c6a774d6ef047d4837f610279d1aacf677ee0db9
You Gao <djx3rn@virginia.edu>
2025-04-07 20:09:12 -0400

    feat: that was easy

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ragebaiter
 funny discord bot
+
+## understanding discord bot architecture
+
+1. Bot Token is presented for a websocket
+2. Discord sends events via the websocket
+3. discord.py defines client methods and configs
+4. Python's async/await that is built on-top of coroutines and non-blocking i/o
+5. make something cool
+
+## notes to self
+
+* remember to await any operation that seems like i/o
---
+++ bot_llm.py
@@ -0,0 +1,25 @@
+import discord
+import os, random
+
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+RESPONSES = open('responses.txt').readlines()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        global RESPONSES
+        channel = message.channel
+        response = random.choice(RESPONSES).strip()
+        if message.author == self.user:
+            return
+        await channel.send(response)
+        print(f'Sent message: {response}')
+         
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
